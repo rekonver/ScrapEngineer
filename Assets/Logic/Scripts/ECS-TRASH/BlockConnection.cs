1e6d1c924da56cb3c1a30899c8ed3e6c1eebde0e
rekonver <127688505+rekonver@users.noreply.github.com>
2025-07-25 18:40:10 +0200

    Delete Phys ECS

--- Assets/Logic/Scripts/ECS-TRASH/BlockConnection.cs
+++
@@ -1,6 +0,0 @@
-using Unity.Entities;
-
-public struct BlockConnection : IBufferElementData
-{
-    public int ConnectedBlockId;
-}
--- Assets/Logic/Scripts/ECS-TRASH/GroupId.cs
+++
@@ -1,6 +0,0 @@
-using Unity.Entities;
-
-public struct GroupId : IComponentData
-{
-    public int Value;
-}
